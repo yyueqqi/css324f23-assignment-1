36d9620b04c0ac30eba68406d7106e35da24ca5a
namwan <130913457+yyueqqi@users.noreply.github.com>
2023-09-11 01:16:23 +0700

    members.py

--- task-0/members.py
+++ task-0/members.py
@@ -1,4 +1,4 @@
 [
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
+    {"name": "Chanikarn Wanla", "sid": "6422770766"},
+    {"name": "Kanyarat Monklung", "sid": "6422771194"}
 ]
